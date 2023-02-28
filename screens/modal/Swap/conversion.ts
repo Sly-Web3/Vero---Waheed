ba4c0cb3605c7a3e66f1c59f03f1ac78c7a13992
Sly <95333171+Sly-Web3@users.noreply.github.com>
2023-02-28 21:58:15 +1300

    Add files via upload

---
+++ screens/modal/Swap/conversion.ts
@@ -0,0 +1,16 @@
+import { BigNumberish, ethers } from 'ethers'
+
+const READABLE_FORM_LEN = 4
+
+export function fromReadableAmount(
+  amount: number,
+  decimals: number
+): BigNumberish {
+  return ethers.utils.parseUnits(amount.toString(), decimals)
+}
+
+export function toReadableAmount(rawAmount: number, decimals: number): string {
+  return ethers.utils
+    .formatUnits(rawAmount, decimals)
+    .slice(0, READABLE_FORM_LEN)
+}
